35ef12de2e731be83cc7de310b82116137347fc1
Lemaitre Enzo <lemaitreenzo05@gmail.com>
2026-05-21 15:27:37 +0200

    Refactor: enhance utility functions and improve component creation script

--- backend/utils/slugHelper.ts
+++ backend/utils/slugHelper.ts
@@ -1,12 +1,24 @@
-const getSlug = (value: string) => {
-    return value
-        .toLowerCase()
-        .normalize('NFD')
-        .replace(/[\u0300-\u036f]/g, '')
-        .replace(/[^\w\s-]/g, '')
-        .replace(/\s+/g, '-')
-        .replace(/-+/g, '-')
-        .replace(/^-|-$/g, '')
+/**
+ * Transforms a string into a valid slug.
+ *
+ * @example
+ * getSlug("My company"); // "my-company"
+ * getSlug("hello"); // "hello"
+ * getSlug("HelloWorld"); // "hello-world"
+ * getSlug("HelloWorld"); // "hello-world"
+ *
+ * @param {string} value The string to transform
+ * @returns The slug modified
+ */
+const getSlug = (value: string): string => {
+  return value
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/[^\w\s-]/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-")
+    .replace(/^-|-$/g, "");
 };
 
 export default getSlug;
